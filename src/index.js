3e419bd864c74316cd0dff6260cc409c4554151e
sujeetgithub7 <kumarguptasujeet562@gmail.com>
2025-01-26 03:21:12 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,59 @@
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Connect to MongoDB
+//not a good practice since db is in another continent
+//use try catch and async await for better error handling
+//mongoose.connect(`mongodb://localhost/${DB_NAME}`, {
+
+
+/*
+//this approach is good but index.js file is polluted
+
+import express from 'express';
+const app = express();
+
+// ()() is a self invoking function used below 
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.
+        MONGO_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log('Error connecting to MongoDB:', error);
+            throw error;
+        });
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });  
+    } catch (error) {
+        console.log('Error connecting to MongoDB:', error);
+        throw error;
+    }
+})()
+
+
+*/
